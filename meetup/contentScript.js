f6254c62eb53e5abee49b28a051583b1e2086c27
tahid02 <tanzim1463@gmail.com>
2022-07-25 21:48:14 +0600

    button showing

--- meetup/contentScript.js
+++ meetup/contentScript.js
@@ -1,11 +1,22 @@
 (() => {
   window.addEventListener("load", myMain, false);
+  ////////////////////////////////////////////////////////////////////// check if viewed  ///////////////////////////////////////////
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 
   //////////////////////////////////////////////////////////////// event data  /////////////////////////////////////////////////////////////
   const eventInfo = (eventElement) => {
     //  group name
-    // const groupName = eventElement.getElementsByTagName("p")[1].innerText;
-    eventElement.getElementsByTagName("p")[1].innerHtml = "<div>bitch</div>";
+    const groupName = eventElement.getElementsByTagName("p")[1].innerText;
+
     // no of  attendee
     const noOfAttendee =
       eventElement.querySelectorAll(".hidden").length < 3
@@ -41,10 +52,13 @@
     const autoLoad = setInterval(function () {
       let lastScrollHeight = 0;
       const sh = document.documentElement.scrollHeight;
+      const allLoaded = isInViewport(document.getElementById("main_footer"));
       if (sh != lastScrollHeight) {
         lastScrollHeight = sh;
         document.documentElement.scrollTop = sh;
-      } else {
+      }
+      if (allLoaded) {
+        // if (allLoaded) {
         console.log("loaded");
         clearInterval(autoLoad);
 
