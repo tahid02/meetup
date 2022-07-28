1117499df65fcb0353229843b48f42a598235677
tahid02 <tanzim1463@gmail.com>
2022-07-28 19:25:14 +0600

    still n attendee prob but filter change added

--- meetup/contentScript.js
+++ meetup/contentScript.js
@@ -236,6 +236,18 @@ function myMain(evt) {
 }
 (() => {
   window.addEventListener("load", myMain, false);
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    // listen for messages sent from background.js
+    if (request.message === "hello!") {
+      console.log(request.url); // new url is now in content scripts!
+      location.reload();
+      return false;
+    }
+  });
   // const dateBtn = document.getElementById("day-filter-drop-down");
   // dateBtn.addEventListener("click", myMain);
 })();
